0ae6bcd8cb716e0a06225de227c9c345d83ff234
ccdkevin <ccdck0919@gmail.com>
2017-10-02 10:56:49 +0800

    homework 2

--- honghwaLessionMVC/honghwaLessionMVC/Controllers/AccountingController.cs
+++ honghwaLessionMVC/honghwaLessionMVC/Controllers/AccountingController.cs
@@ -4,28 +4,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using honghwaLessionMVC.Models;
 
 namespace honghwaLessionMVC.Controllers
 {
     public class AccountingController : Controller
     {
-        
+        private SkillTreeHomeworkEntities db = new SkillTreeHomeworkEntities();
+
         public ActionResult ShowData()
         {
-            var dataList = new List<ShowDataModel>();
-            Random rd = new Random();
-            string[] selectValue = { "支出", "收入" };
-
-            for (int x = 0; x < 1000; x++) {
-                int selectValueIndex = rd.Next(0, selectValue.Length);
-                DateTime rdDate = DateTime.UtcNow.AddDays(rd.Next(100));
-                dataList.Add(new ShowDataModel()
+            var dataList = new List<ShowDataViewModel>();
+            var result = (from x in db.AccountBook select x).ToList();
+            for (int x = 0; x < result.Count; x++)
+            {
+                dataList.Add(new ShowDataViewModel
                 {
                     ItemNo = x + 1,
-                    Category = selectValue[selectValueIndex],
-                    Date = rdDate,
-                    Money = rd.Next(50000),
-                    Remark = ""
+                    Category = Convert.ToString(result[x].Categoryyy) == "0" ? "收入" : "支出",
+                    Money = result[x].Amounttt,
+                    Date = result[x].Dateee
                 });
             }
             return View(dataList);
--- honghwaLessionMVC/honghwaLessionMVC/Models/ViewModels/KeyinViewModel.cs
+++ honghwaLessionMVC/honghwaLessionMVC/Models/ViewModels/KeyinViewModel.cs
@@ -1,23 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace honghwaLessionMVC.Models.ViewModels
 {
-    public class ShowDataModel
+    public class KeyinViewModel
     {
-        public int ItemNo { get; set; }
-
         public string Category { get; set; }
-
-        public int Money { get; set; }
-
-        [DisplayFormat(DataFormatString ="{0:d}")]
         public DateTime Date { get; set; }
-
         public string Remark { get; set; }
-
     }
 }
